667bd6ffa9de0d733c72e13da97304edc068792e
Nikolay Turnaviotov <foxmuldercp@users.noreply.github.com>
2016-11-21 22:55:44 +0200

    fucking js

--- src/actions/viewer.jsx
+++ src/actions/viewer.jsx
@@ -1,32 +1,29 @@
 export function fetchViewerProps(login) {
   return function (dispatch, getState) {
-
     const {viewer} = getState()
-
-    if (!viewer.token){
-      fetch('https://sites.mulder.kiev.ua/api/v1/auth/is_signed_in?user_token=LiJPFGXRktQGHpyFxzNs', {
-      method: 'GET',
-    })
-    .then(response => response.json())
-    .then(json => dispatch({
-       type:'auth_success',
-       payload:{
-         ...json
-       }
-    }))
-   }
- }
-}
+    console.log ('action viewer', viewer)
+//    if (!viewer.token){
+//    var url = 'https://sites.mulder.kiev.ua/api/v1/users/sign_in.json';
+//    fetch(url, {
+//      mode: 'no-cors',
+//      credentials: 'include',
+//      headers: {
+//        'Accept': 'application/json',
+//        'Content-Type': 'application/json',
+//        'csrf-param': this.state.forgery_name,
+//        'X-CSRF-Token': this.state.auth_token
+//      },
+//      method: 'POST',
+//      body: JSON.stringify({user: {email: viewer.email, password: viewer.password}})
+//  })
+//  .then(response => response.json())
+//  .then(json => dispatch({
+//     type:'auth_success',
+//     payload:{
+//       ...json
+//     }
+//  }))
+}} //}
 
 //'{"user":{"email":"test@example.com","password":"password"}}' -H 'Content-Type:application/json'
 // http://test.mulder.kiev.ua/api/v1/users/sign_in.json
-/*fetch(url, {
-    mode: 'same-origin',
-    credentials: 'same-origin',
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-    },
-    method: 'POST',
-    body: {_method: '_delete', domain: {id: this.state.item.id}, authenticity_token: this.state.auth_token}
-*/
--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -2,50 +2,63 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 
 import {fetchViewerProps} from '../actions/viewer'
-import TextField from 'material-ui/TextField'
-import Button from 'material-ui/RaisedButton'
+
+import TextField from "material-ui/TextField"
+import Button from "material-ui/RaisedButton"
 
 class Login extends Component {
 
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            login:    '',
-            password: ''
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      email:    "",
+      password: ""
     }
+  }
 
-    handleChange = (event) => {
-        this.setState({
-            value: event.target.value
-        })
-    }
+  /*  componentDidMount(){
+    this.props.dispatch(fetchViewerProps())
+  }*/
 
-    render(){
-        return <div>
-            <TextField
-                id="login"
-                value={this.state.login}
-                onChange={this.handleChange}
-                hintText="Your E-mail"
-                floatingLabelText="E-Mail"
-            />
-            <TextField
-                id="password"
-                hintText="Password"
-                floatingLabelText="Password"
-                type="password"
-            /><br />
-            <Button
-                label="Sign in"
-                primary={true}
-            />
-        </div>
-    }
+  handleChange = (event) => {
+    console.log(this.state)
+    this.setState({...this.state, [event.target.name]: event.target.value})
+  }
+
+  fetchData(){
+    this.props.dispatch(fetchViewerProps())
+  }
+
+  render(){
+    return <div>
+      <TextField
+         id="login"
+         name="email"
+         value={this.state.email}
+         onChange={this.handleChange}
+         hintText="Your E-mail"
+         floatingLabelText="E-Mail"
+      />
+      <TextField
+         id="password"
+         name="password"
+         value={this.state.password}
+         onChange={this.handleChange}
+         hintText="Password"
+         floatingLabelText="Password"
+         type="password"
+      /><br />
+      <Button
+         label="Sign in"
+         primary={true}
+         onClick={() => this.fetchData()}
+      />
+    </div>
+  }
+
 }
 
 export default Login
-//= connect(store => ({
+// = connect(store => ({
 //    token: store.viewer.token,
-//}))(User)
+// }))(User)
