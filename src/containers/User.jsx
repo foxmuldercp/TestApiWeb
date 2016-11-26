6c9234d23d08618e014d2b4759df292810983fcd
Nikolay Turnaviotov <foxmuldercp@users.noreply.github.com>
2016-11-26 03:17:49 +0200

    Lock User component for non-authenticated users

--- src/containers/User.jsx
+++ src/containers/User.jsx
@@ -1,28 +1,29 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 
+import {push} from 'react-router-redux'
+
 import {fetchViewerProps} from '../actions/viewer'
+import Button from 'material-ui/RaisedButton'
 
 class User extends Component {
 
-  /*  componentDidMount(){
-    this.props.dispatch(fetchViewerProps())
-    }*/
-
-    fetchData(){
-        this.props.dispatch(fetchViewerProps())
+  componentDidMount(){
+    if (!this.props.email && !this.props.token) {
+      this.props.dispatch(push('/login'))
     }
+  }
 
-    render(){
-        const {params, token} = this.props
-        const {id} = params
+  render(){
+    const {params, token, email} = this.props
+    const {id} = params
 
-        return <div>
-            <div>{token}</div>
-        </div>
-    }
+    return <div>
+      <div>{email}</div>
+   </div>
+  }
 }
 
 export default User = connect(store => ({
-    token: store.viewer.token,
+  token: store.viewer.token, email: store.viewer.email
 }))(User)
