050b780cf0cf58d099e20bf8ae272082d8d9e09e
Nikolay Turnaviotov <foxmuldercp@users.noreply.github.com>
2016-11-26 02:10:49 +0200

    Drop obsolete code, add reading auth token from localStore

--- src/App.jsx
+++ src/App.jsx
@@ -5,64 +5,48 @@ import {combineReducers, applyMiddleware, createStore, compose} from 'redux'
 import {Provider} from 'react-redux'
 import {syncHistoryWithStore, routerReducer, routerMiddleware} from 'react-router-redux'
 import thunk from 'redux-thunk'
-import 'muicss/dist/css/mui.min.css'
-require("font-awesome-webpack");
-
-// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
-//import {createDevTools} from 'redux-devtools'
-//import DockMonitor from 'redux-devtools-dock-monitor'
-//import LogMonitor from 'redux-devtools-log-monitor'
-//import Inspector from 'redux-devtools-inspector'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import routes from './routes'
 import Reducers from './Reducers'
 
-// import injectTapEventPlugin from 'react-tap-event-plugin'
+import injectTapEventPlugin from 'react-tap-event-plugin'
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
-// injectTapEventPlugin()
-
-// createDevTools takes a monitor and produces a DevTools component
-/*const DevTools = createDevTools(
-    // Monitors are individually adjustable with props.
-    // Consult their repositories to learn about those props.
-    // Here, we put LogMonitor inside a DockMonitor.
-    // Note: DockMonitor is visible by default.
-    <DockMonitor
-        toggleVisibilityKey='ctrl-h'
-        changePositionKey='ctrl-q'
-        defaultIsVisible={true}
-    >
-        <LogMonitor />
-    </DockMonitor>
-);*/
+injectTapEventPlugin()
 
 const middleware = compose(
-    applyMiddleware(
-        thunk,
-        routerMiddleware(hashHistory)
-    ),
-    //DevTools.instrument()
+  applyMiddleware(
+    thunk,
+    routerMiddleware(hashHistory)
+  ),
 )
 
+function getStoreInitialState() {
+  return {viewer: {email: localStorage.getItem('email'), token: localStorage.getItem('token') }}
+}
+
 const store = createStore(
-    combineReducers({
-        ...Reducers,
-        routing: routerReducer
-    }),
-    middleware
+  combineReducers({
+    ...Reducers,
+    routing: routerReducer
+  }),
+  getStoreInitialState(),
+  middleware
 )
 
 const history = syncHistoryWithStore(hashHistory, store)
 
 export default class App extends Component {
-    render() {
-        return <Provider store={store}>
-                <Router history={history}>
-                    {routes}
-                </Router>
-        </Provider>
-    }
+
+  render() {
+    return <Provider store={store}>
+      <MuiThemeProvider>
+        <Router history={history}>
+          {routes}
+        </Router>
+      </MuiThemeProvider>
+    </Provider>
+  }
 }
