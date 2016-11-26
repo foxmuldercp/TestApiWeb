0c80d480666abba8a0d37663cb4b139670d9319c
Nikolay Turnaviotov <foxmuldercp@users.noreply.github.com>
2016-11-26 02:17:39 +0200

    Update NavBar for Material UI inputs, added LogOff support

--- src/containers/NavBar.jsx
+++ src/containers/NavBar.jsx
@@ -2,39 +2,63 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {push} from 'react-router-redux'
 
-import Appbar from 'muicss/lib/react/appbar'
-import Row from 'muicss/lib/react/row'
-import Col from 'muicss/lib/react/col'
-import Button from 'muicss/lib/react/button'
+import AppBar from 'material-ui/AppBar'
+import IconButton from 'material-ui/IconButton'
+import IconMenu from 'material-ui/IconMenu'
+import MenuItem from 'material-ui/MenuItem'
+import FlatButton from 'material-ui/FlatButton'
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert'
+import NavigationClose from 'material-ui/svg-icons/navigation/close'
 
-import Icon from './Icon'
+import {logOut} from '../actions/viewer'
 
-class NavBar extends Component {
 
-  state = {
-    email: this.props.email,
-    token: this.props.token,
-    logged: true
+class LoginBtn extends Component {
+  static muiName = 'FlatButton';
+
+
+  render() {
+//    const {dispatch} = this.props
+    return (<div>
+      <FlatButton {...this.props} label="Login" onClick={() => this.props.dispatch(push('/login'))} />
+      <FlatButton {...this.props} label="Register" />
+    </div>);
   }
+}
+
+class Logged extends Component {
+  render() {
+  return <IconMenu
+    iconButtonElement={
+      <IconButton><MoreVertIcon /></IconButton>
+    }
+    targetOrigin={{horizontal: 'right', vertical: 'top'}}
+    anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+  >
+    <MenuItem primaryText="Refresh" />
+    <MenuItem primaryText="Help" />
+    <MenuItem name='SignOut' primaryText="Sign out" onClick={() => this.props.dispatch(logOut())} />
+  </IconMenu>
+}}
+
+/**
+ * This example is taking advantage of the composability of the `AppBar`
+ * to render different components depending on the application state.
+ */
+ 
+class NavBar extends Component {
 
   render() {
     const email = this.props.email
-    let s2 = {textAlign: 'right'};
-    return <Appbar>
-      <Row>
-        <Col md='3' md-offset='1'>
-          <Button variant="flat" onClick={() => this.props.dispatch(push('/posts'))}><Icon name='file-text-o' /></Button>
-          <Button variant="flat" onClick={() => this.props.dispatch(push('/posts'))}><Icon name='users' /></Button>
-        </Col>
-        <Col md='1' md-offset='1'>
-          <Button variant="flat" onClick={() => this.props.dispatch(push('/'))}><Icon name='home' /></Button>
-        </Col>
-        <Col md='3' md-offset='3'>
-          <Button variant="flat" onClick={() => this.props.dispatch(push('/login'))}><Icon name='sign-in' /></Button>
-          <Button variant="flat" onClick={() => this.props.dispatch(push('/login'))}><Icon name='user-plus' /></Button>
-        </Col>
-      </Row>
-    </Appbar>
+    return (
+      <div>
+        <AppBar
+          title="Title"
+          iconElementLeft={<IconButton><NavigationClose /></IconButton>}
+          iconElementRight={ email ? <div> {email} <Logged {...this.props} /> </div>:<LoginBtn {...this.props} />}
+        />
+      </div>
+    )
   }
 }
 
