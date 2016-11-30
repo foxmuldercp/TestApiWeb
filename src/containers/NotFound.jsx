ef0d55ae5f682a24b56c6a630c15e3ddd246ecf4
Nikolay Turnaviotov <foxmuldercp@users.noreply.github.com>
2016-11-30 17:36:20 +0200

    Add PageNotFound component

--- src/containers/NotFound.jsx
+++ src/containers/NotFound.jsx
@@ -5,10 +5,16 @@ import {push} from 'react-router-redux'
 import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
 import Button from "material-ui/RaisedButton"
 
+import Container from 'muicss/lib/react/container';
+import Row from 'muicss/lib/react/row';
+import Col from 'muicss/lib/react/col';
+
 class NotFound extends Component {
 
   render() {
-    return <Card expanded={true}>
+    return  <Container fluid={true}>
+        <Row><Col xs='12' sm='4' sm-offset='4' md='6' lg='6'>
+            <Card expanded={true}>
     <CardHeader
       title="Page not found"
       subtitle="Seems, that this url is not functional now."
@@ -18,6 +24,9 @@ class NotFound extends Component {
       <Button label="Return to home page" onClick={() => this.props.dispatch(push('/'))}/>
     </CardActions>
   </Card>
+  </Col>
+</Row>
+</Container>
   }
 }
 
