6ad2bbd86990ed50c03c90db5b4d80d7b1bf21bb
Nikolay Turnaviotov <foxmuldercp@users.noreply.github.com>
2016-11-30 17:35:13 +0200

    add grid imtes from muicss and authentication checking with redirect to app root if user is authenticated

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,10 +1,15 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
+import {push} from 'react-router-redux'
 
 import {fetchViewerProps} from '../actions/viewer'
 
 import TextField from "material-ui/TextField"
 import Button from "material-ui/RaisedButton"
+
+import Container from 'muicss/lib/react/container';
+import Row from 'muicss/lib/react/row';
+import Col from 'muicss/lib/react/col';
 
 class Login extends Component {
 
@@ -16,11 +21,11 @@ class Login extends Component {
     }
   }
 
-  /*
-    componentDidMount(){
-    this.props.dispatch(fetchViewerProps(this.state))
+  componentDidMount(){
+    if (this.props.email && this.props.token) {
+      this.props.dispatch(push('/'))
+    }
   }
-  */
 
   handleChange = (event) => {
     this.setState({...this.state, [event.target.name]: event.target.value})
@@ -30,35 +35,51 @@ class Login extends Component {
     this.props.dispatch(fetchViewerProps(this.state))
   }
 
-  render(){
-    return <div>
-      <TextField
-         id="login"
-         name="email"
-         value={this.state.email}
-         onChange={this.handleChange}
-         hintText="Your E-mail"
-         floatingLabelText="E-Mail"
-      />
-      <TextField
-         id="password"
-         name="password"
-         value={this.state.password}
-         onChange={this.handleChange}
-         hintText="Password"
-         floatingLabelText="Password"
-         type="password"
-      /><br />
-      <Button
-         label="Sign in"
-         primary={true}
-         onClick={() => this.fetchData()}
-      />
-    </div>
+  render() {
+    return <Container fluid={true}>
+      <Row>
+        <Col xs='12' sm='4' sm-offset='4' md='6' lg='6'>
+        <h2 class='roboto'>Login</h2>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs='12' sm='4' sm-offset='4' md='6' lg='6'>
+          <TextField
+            id="login"
+            name="email"
+            value={this.state.email}
+            onChange={this.handleChange}
+            hintText="Your E-mail"
+            floatingLabelText="E-Mail"
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col xs='12' sm='4' sm-offset='4' md='6' lg='6'>
+          <TextField
+            id="password"
+            name="password"
+            value={this.state.password}
+            onChange={this.handleChange}
+            hintText="Password"
+            floatingLabelText="Password"
+            type="password"
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col xs='12' sm='4' sm-offset='4' md='6' lg='6'>
+          <Button
+            label="Sign in"
+            primary={true}
+            onClick={() => this.fetchData()}
+          />
+        </Col>
+      </Row>
+    </Container>
   }
-
 }
 
 export default Login = connect(store => ({
-    token: store.viewer.token, token: store.viewer.email
+    token: store.viewer.token, email: store.viewer.email
 }))(Login)
